fd2fb60bd6c6b4564c31e21ae880a8249650fffe
Rudra <rudrapratapsingh@gmail.com>
2025-10-07 10:44:13 +0530

    Updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+const viteTsConfigPaths = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
